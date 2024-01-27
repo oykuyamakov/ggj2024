af79e2f99198c697ffc3c0dd707583c87233e5d2
oykuyamak <oykuyamak@gmail.com>
2024-01-27 18:21:17 -0500

    bathroom scene, and start of character implementation

---
+++ Assets/Scripts/Mechanics/BathroomSceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathroomSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> m_BackgroundSprites;
+    
+    private int m_CurrentBackgroundIndex = 0;
+    
+}
