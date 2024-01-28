fc61b8892c7d112607ebeb4d6883a54e83cd09fc
oykuyamak <oykuyamak@gmail.com>
2024-01-28 13:43:02 -0500

    grandma image added, char selection size is fixed?

--- Assets/Scripts/CharSelectImage.cs
+++ Assets/Scripts/CharSelectImage.cs
@@ -22,6 +22,8 @@ public class CharSelectImage : MonoBehaviour,IPointerEnterHandler, IPointerExitH
         //m_CharImage.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
         
         m_CharImage.sprite = m_Char.SelectedAnimationSprites.First();
+        
+        m_CharImage.transform.localScale = new Vector3(2f, 2f, 2f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -39,7 +41,7 @@ public class CharSelectImage : MonoBehaviour,IPointerEnterHandler, IPointerExitH
             return;
         
         
-        m_CharImage.transform.localScale = new Vector3(2f, 2f, 2f);
+        m_CharImage.transform.localScale = new Vector3(2.2f, 2.2f, 2.2f);
         
         GameManager.Instance.ChangeCurrentCharacter(m_Char);
     }
